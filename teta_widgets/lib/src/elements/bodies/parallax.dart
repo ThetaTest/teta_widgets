f5ddf882efabe22d85ff42db7712bc3257b674fd
AntonioVerdiglione1996 <anto.verdi27@gmail.com>
2022-03-25 14:52:14 +0100

    removed parallax from widget list

--- teta_widgets/lib/src/elements/bodies/parallax.dart
+++ teta_widgets/lib/src/elements/bodies/parallax.dart
@@ -36,7 +36,7 @@ final parallaxIntrinsicStates = IntrinsicStates(
   advicedChildrenCanHaveAtLeastAChild: [],
   displayName: NodeType.name(_globalType),
   type: _globalType,
-  category: NodeCategories.advanced,
+  category: NodeCategories.unclassified,
   maxChildren: 1,
   canHave: ChildrenEnum.child,
   addChildLabels: [],
