ef918cb0bd1682fff5f599ae43b2269017aa8e88
AntonioVerdiglione1996 <anto.verdi27@gmail.com>
2022-05-16 11:25:07 +0200

    feat(new stripe products builder)

---
+++ teta_widgets/lib/src/elements/code/templates/stripe_products_builder.dart
@@ -0,0 +1,68 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+// Project imports:
+import 'package:teta_widgets/src/elements/controls/key_constants.dart';
+import 'package:teta_widgets/src/elements/features/dataset.dart';
+import 'package:teta_widgets/src/elements/nodes/node.dart';
+import 'package:teta_widgets/src/elements/nodes/node_body.dart';
+
+/// Generates the code for ListView.builder widget
+///
+/// Returns:
+/// ```dart
+/// Builder(
+///   builder: (context) {
+///     final children = <Widget>[]; // node's children
+///     return ListView.builder(
+///       scrollDirection: Axis.vertical,
+///       itemCount: children.length,
+///       itemBuilder: (context, index) {
+///         return children[index];
+///       },
+///     );
+///   },
+/// );
+/// ```
+String stripeProductsBuilderCodeTemplate(
+  final BuildContext context,
+  final NodeBody body,
+  final CNode? child,
+) {
+  final _scrollDirection =
+      !(body.attributes[DBKeys.isVertical] as bool? ?? false)
+          ? 'scrollDirection: Axis.horizontal,'
+          : '';
+  final shrinkWrap = body.attributes[DBKeys.flag] as bool? ?? false;
+  final childString = child != null ? child.toCode(context) : '';
+  // final dataset =
+  //     (body.attributes[DBKeys.datasetInput] as FDataset).datasetName;
+  return '''
+    Builder(
+      builder: (context) {
+        return ListView.builder(
+          $_scrollDirection
+          shrinkWrap: $shrinkWrap,
+          itemCount: this.stripeProductsList.length > 0 ? this.stripeProductsList.length : 0,
+          itemBuilder: (context, index) {
+            return $childString;
+          },
+        );
+      },
+    );
+  ''';
+}
+
+// ignore: unused_element
+Widget _example() {
+  return Builder(
+    builder: (final context) {
+      final children = <Widget>[];
+      return ListView.builder(
+        itemCount: children.length,
+        itemBuilder: (final context, final index) {
+          return children[index];
+        },
+      );
+    },
+  );
+}
--- teta_widgets/lib/src/elements/nodes/enum.dart
+++ teta_widgets/lib/src/elements/nodes/enum.dart
@@ -119,6 +119,7 @@ enum NType {
   supabaseLoggedUser,
   //Stripe
   stripeProductsList,
+  stripeProductsBuilder,
 
   // RevenueCat
   revenueCatProductsList,
