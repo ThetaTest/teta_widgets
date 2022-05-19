d5e4d62924aedca252faba2a851b72005eb5bda3
AntonioVerdiglione1996 <anto.verdi27@gmail.com>
2022-05-19 14:49:46 +0200

    fix(map to code fixed)

--- teta_widgets/lib/src/elements/code/templates/component.dart
+++ teta_widgets/lib/src/elements/code/templates/component.dart
@@ -4,6 +4,7 @@
 // Package imports:
 // Flutter imports:
 import 'package:diacritic/diacritic.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recase/recase.dart';
@@ -13,8 +14,8 @@ import 'package:teta_widgets/src/elements/controls/key_constants.dart';
 import 'package:teta_widgets/src/elements/index.dart';
 import 'package:teta_widgets/src/elements/nodes/node_body.dart';
 
-/// Component Template
-String componentCodeTemplate(
+///Old Component Template
+String componentCodeTemplateOld(
   final BuildContext context,
   final NodeBody body,
   final List<CNode> children,
@@ -24,6 +25,7 @@ String componentCodeTemplate(
 
   final projectLoaded =
       BlocProvider.of<FocusProjectBloc>(context).state as ProjectLoaded;
+
   //!this fix an error of badstate
   if (body.attributes[DBKeys.componentName] == null ||
       (projectLoaded.prj.pages ?? <PageObject>[]).indexWhere(
@@ -35,6 +37,7 @@ String componentCodeTemplate(
   final compWidget = projectLoaded.prj.pages!.firstWhere(
     (final element) => element.name == body.attributes[DBKeys.componentName],
   );
+
   //todo:external parameters implementation
   //used to prepare parmas in uri for loading url?
   //prepareParamsForUi(compWidget, body);
@@ -95,24 +98,20 @@ String componentCodeTemplate(
     final parametersString = StringBuffer()..write('');
     final stringParamsToSend = StringBuffer()..write('');
     for (final param in compWidget.params) {
-      if ("${paramsToSend?[param.id]?['dataset']}" == 'States' ||
-          "${paramsToSend?[param.id]?['dataset']}" == 'Params') {
-        final valueToSend = (paramsToSend ?? <String, dynamic>{})[param.id]
-                ?['label'] as String? ??
-            'null';
-        if (valueToSend != 'null') {
-          final name = ReCase(param.name);
-          stringParamsToSend.write('${name.camelCase}: ');
-          final rc = ReCase(valueToSend);
-          stringParamsToSend.write('${rc.camelCase}, ');
-        }
-      } else {
-        final name = ReCase(param.name);
-        stringParamsToSend.write('${name.camelCase}: ');
-        final valueToSend =
-            "datasets['${paramsToSend?[param.id]?['dataset']}'][index]['${paramsToSend?[param.id]?['label']}']";
-        stringParamsToSend.write('$valueToSend, ');
+      final rc = ReCase(param.name);
+      var value = param.typeDeclaration(rc.camelCase) == 'String'
+          ? "'${param.get}'"
+          : param.firstValueForInitialization();
+      final valueToSend = (paramsToSend ?? <String, dynamic>{})[param.id]
+              ?['label'] as String? ??
+          'null';
+      Logger.printWarning(valueToSend);
+      if (valueToSend != 'null') {
+        final rc = ReCase(valueToSend);
+        stringParamsToSend.write('${rc.camelCase}, ');
+        value = rc.camelCase;
       }
+      parametersString.write('${rc.camelCase}: $value,');
     }
 
     // final stringParamsToSend = StringBuffer()..write('');
@@ -184,6 +183,69 @@ String componentCodeTemplate(
   //     ${stringParamsToSend.toString()}
   //   )
   //   ''';
+}
+
+/// Component Template
+String componentCodeTemplate(
+  final BuildContext context,
+  final NodeBody body,
+  final List<CNode> children,
+  final int pageId,
+) {
+  final projectLoaded =
+      BlocProvider.of<FocusProjectBloc>(context).state as ProjectLoaded;
+
+  //!this fix an error of badstate
+  if (body.attributes[DBKeys.componentName] == null ||
+      (projectLoaded.prj.pages ?? <PageObject>[]).indexWhere(
+            (final element) =>
+                element.name == body.attributes[DBKeys.componentName],
+          ) ==
+          -1) return '';
+
+  final compWidget = projectLoaded.prj.pages!.firstWhere(
+    (final element) => element.name == body.attributes[DBKeys.componentName],
+  );
+  final pageName = body.attributes[DBKeys.componentName] as String;
+  final paramsToSend =
+      body.attributes[DBKeys.paramsToSend] as Map<String, dynamic>?;
+  final temp = removeDiacritics(
+    pageName
+        .replaceFirst('0', 'A0')
+        .replaceFirst('1', 'A1')
+        .replaceFirst('2', 'A2')
+        .replaceFirst('3', 'A3')
+        .replaceFirst('4', 'A4')
+        .replaceFirst('5', 'A5')
+        .replaceFirst('6', 'A6')
+        .replaceFirst('7', 'A7')
+        .replaceFirst('8', 'A8')
+        .replaceFirst('9', 'A9')
+        .replaceAll(' ', '')
+        .replaceAll("'", '')
+        .replaceAll('"', ''),
+  );
+  final pageNameRC = ReCase(temp);
+  //params
+  final parametersString = StringBuffer()..write('');
+  final stringParamsToSend = StringBuffer()..write('');
+  for (final param in compWidget.params) {
+    final rc = ReCase(param.name);
+    var value = param.typeDeclaration(rc.camelCase) == 'String'
+        ? "'${param.get}'"
+        : param.firstValueForInitialization();
+    final valueToSend =
+        (paramsToSend ?? <String, dynamic>{})[param.id]?['label'] as String? ??
+            'null';
+    Logger.printWarning(valueToSend);
+    if (valueToSend != 'null') {
+      final rc = ReCase(valueToSend);
+      stringParamsToSend.write('${rc.camelCase}, ');
+      value = rc.camelCase;
+    }
+    parametersString.write('${rc.camelCase}: $value,');
+  }
+  return 'Page${pageNameRC.pascalCase}(${parametersString.toString()})';
 }
 
 String prepareParamsForUi(final PageObject page, final NodeBody body) {
