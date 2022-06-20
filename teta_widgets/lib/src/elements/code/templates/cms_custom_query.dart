6c4d4efcf136d45316a79487eafe4990dc4cc218
andreabuttarelli <andreabuttarelli.design@gmail.com>
2022-06-20 14:31:57 +0200

    feat(Custom query toCode)

---
+++ teta_widgets/lib/src/elements/code/templates/cms_custom_query.dart
@@ -0,0 +1,47 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+// Project imports:
+import 'package:teta_widgets/src/elements/controls/key_constants.dart';
+import 'package:teta_widgets/src/elements/features/text_type_input.dart';
+import 'package:teta_widgets/src/elements/nodes/dynamic.dart';
+import 'package:teta_widgets/src/elements/nodes/node.dart';
+
+/// Generates the code for Padding widget
+String cmsCustomQueryCodeTemplate(
+  final BuildContext context,
+  final NDynamic node,
+  final List<CNode> children,
+  final int? loop,
+) {
+  final query =
+      (node.body.attributes[DBKeys.value] as FTextTypeInput).toCode(loop);
+
+  var child = 'const SizedBox()';
+  if (children.isNotEmpty) {
+    child = children.first.toCode(context);
+  }
+  var loader = 'const Center(child: CircularProgressIndicator(),)';
+  if (children.length >= 2) {
+    loader = children[1].toCode(context);
+  }
+  final func = '''
+  final list = (snapshot.data as TetaResponse?)?.data as List<dynamic>? ?? <dynamic>[];
+  datasets['${node.name ?? node.intrinsicState.displayName}'] = list ?? const <dynamic>[];
+  const index = 0;
+  ''';
+
+  return '''
+  TetaFutureBuilder(
+    future: TetaCMS.instance.client.customQuery(
+      $query,
+    ),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) {
+        return $loader;
+      }
+      $func
+      return $child;
+    }
+  )
+  ''';
+}
