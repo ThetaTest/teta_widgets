55f5f634e74f8c208c0a1baa242a03724baeffb7
AntonioVerdiglione1996 <anto.verdi27@gmail.com>
2022-07-26 15:16:58 +0200

    fix(concentricpageview code fix, placeholder  support new dark/light)

--- teta_widgets/lib/src/elements/code/templates/concentric_page_view.dart
+++ teta_widgets/lib/src/elements/code/templates/concentric_page_view.dart
@@ -1,6 +1,5 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-
 // Project imports:
 import 'package:teta_widgets/src/elements/code/snippets.dart';
 import 'package:teta_widgets/src/elements/controls/key_constants.dart';
@@ -8,19 +7,30 @@ import 'package:teta_widgets/src/elements/features/fill.dart';
 import 'package:teta_widgets/src/elements/nodes/node.dart';
 import 'package:teta_widgets/src/elements/nodes/node_body.dart';
 
-/// PageView Template
+/// Concentric PageView Template
 String concentricPageViewCodeTemplate(
   final BuildContext context,
   final NodeBody body,
   final CNode node,
   final List<CNode> children,
 ) {
-  final fill = (body.attributes[DBKeys.fill] as FFill).getHexColor(context);
-
+  final fill = FFill.toCode(
+    body.attributes[DBKeys.fill] as FFill,
+    context,
+    flagConst: false,
+  );
+  final childrenStr =
+      CS.children(context, children).replaceAll('children:', '');
   return '''
     ConcentricPageView(
-      color: Color(0xFF$fill),    
-      ${CS.children(context, children)}
+      colors: [
+        ${fill!.replaceAll('color:', '')}
+        ${fill.replaceAll('color:', '')}
+      ],
+      itemCount: ${children.length},
+      itemBuilder: (final int index) {
+        return ${childrenStr.substring(0, childrenStr.length - 1)}[index];
+      },
     )
   ''';
 }
