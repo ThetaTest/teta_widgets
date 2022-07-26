3cb164c1c116cd664e6dee285fe4810032e2542c
AntonioVerdiglione1996 <anto.verdi27@gmail.com>
2022-07-26 12:53:16 +0200

    fix(circularindicator  support new dark/light)

--- teta_widgets/lib/src/elements/widgets/circular_progress_indicator.dart
+++ teta_widgets/lib/src/elements/widgets/circular_progress_indicator.dart
@@ -1,5 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 // Package imports:
 import 'package:teta_core/teta_core.dart';
 // Project imports:
@@ -32,13 +33,29 @@ class WCircularProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
+    final isLight = BlocProvider.of<PaletteDarkLightCubit>(context).state;
+    PaletteModel? model;
+    BlocProvider.of<PaletteBloc>(context).state.forEach((final element) {
+      if (element.id == fill.paletteStyle) model = element;
+    });
     return NodeSelectionBuilder(
       node: node,
       forPlay: forPlay,
       child: CircularProgressIndicator(
         valueColor:
-            AlwaysStoppedAnimation<Color>(HexColor(fill.getHexColor(context))),
+            AlwaysStoppedAnimation<Color>(_getCircularColor(model, isLight)),
       ),
     );
+  }
+
+  // ignore: avoid_positional_boolean_parameters
+  HexColor _getCircularColor(final PaletteModel? model, final bool isLight) {
+    if (model != null) {
+      return isLight
+          ? HexColor(model.light!.levels!.first.color)
+          : HexColor(model.fill!.levels!.first.color);
+    } else {
+      return HexColor(fill.levels!.first.color);
+    }
   }
 }
