82cec1f1c6aa1f405de8fdee59ac947bbd8cbc07
AntonioVerdiglione1996 <anto.verdi27@gmail.com>
2022-08-18 16:46:30 +0200

    fix(pageview)

--- teta_widgets/lib/src/elements/controls/atoms/action_element.dart
+++ teta_widgets/lib/src/elements/controls/atoms/action_element.dart
@@ -663,7 +663,8 @@ class ActionElementControlState extends State<ActionElementControl> {
                           .where(
                             (final element) =>
                                 element.type == VariableType.int ||
-                                element.type == VariableType.double,
+                                element.type == VariableType.double ||
+                                element.type == VariableType.string,
                           )
                           .map((final e) => e.name)
                           .where((final element) => element != 'null')
@@ -679,7 +680,7 @@ class ActionElementControlState extends State<ActionElementControl> {
                     const Padding(
                       padding: EdgeInsets.only(bottom: 8),
                       child: TDetailLabel(
-                        "It requires 'int' or 'double' variables",
+                        "It requires an'int','double' or 'string' variable",
                       ),
                     ),
                   ],
