763760836050cd6d5a987ecfcc77ed4118f27f1d
AntonioVerdiglione1996 <anto.verdi27@gmail.com>
2022-09-14 16:02:55 +0200

    fix(tests broken from unused imports dart.js)

--- teta_widgets/lib/src/elements/bodies/google_maps_body.dart
+++ teta_widgets/lib/src/elements/bodies/google_maps_body.dart
@@ -1,18 +1,11 @@
 // Flutter imports:
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 // Package imports:
-import 'package:teta_core/gen/assets.gen.dart';
-import 'package:teta_core/src/models/dataset.dart';
-import 'package:teta_core/src/models/variable.dart';
 import 'package:teta_core/teta_core.dart';
 import 'package:teta_widgets/src/elements/code/snippets.dart';
 import 'package:teta_widgets/src/elements/code/templates/google_maps_cubit_template.dart';
 import 'package:teta_widgets/src/elements/code/templates/google_maps_template.dart';
-
 // Project imports:
 import 'package:teta_widgets/src/elements/controls/control_model.dart';
 import 'package:teta_widgets/src/elements/controls/key_constants.dart';
