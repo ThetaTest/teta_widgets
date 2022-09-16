9a9e48aa9911f8d5d1637e344e41933117445e90
andreabuttarelli <andreabuttarelli.design@gmail.com>
2022-09-16 11:55:50 +0200

    fix(bottom sheet & open snack params)

--- teta_widgets/lib/src/elements/actions/navigation/open_bottom_sheet.dart
+++ teta_widgets/lib/src/elements/actions/navigation/open_bottom_sheet.dart
@@ -2,6 +2,7 @@
 // ignore_for_file: public_member_api_docs
 
 // Package imports:
+import 'package:camera/camera.dart';
 import 'package:collection/collection.dart';
 import 'package:diacritic/diacritic.dart';
 // Flutter imports:
@@ -10,12 +11,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:recase/recase.dart';
-import 'package:teta_core/src/blocs/focus_project/index.dart';
-import 'package:teta_core/src/models/dataset.dart';
-import 'package:teta_core/src/models/page.dart';
-import 'package:teta_core/src/models/variable.dart';
 import 'package:teta_core/src/rendering/nodes_original.dart';
 import 'package:teta_core/src/repositories/queries/node.dart';
+import 'package:teta_core/teta_core.dart';
 import 'package:teta_widgets/src/elements/code/formatter_test.dart';
 // Project imports:
 import 'package:teta_widgets/src/elements/nodes/node.dart';
@@ -34,7 +32,9 @@ class FActionNavigationOpenBottomSheet {
       if (nameOfPage != null) {
         final prj =
             BlocProvider.of<FocusProjectBloc>(context).state as ProjectLoaded;
-        var page = prj.prj.pages!
+        final currentPage = BlocProvider.of<PageCubit>(context).state;
+        PageObject? page;
+        page = prj.prj.pages!
             .firstWhereOrNull((final element) => element.name == nameOfPage);
         if (page != null) {
           final list = await NodeQueries.fetchNodesByPage(page.id);
@@ -53,7 +53,94 @@ class FActionNavigationOpenBottomSheet {
             context: context,
             builder: (final context) => page!.scaffold!.toWidget(
               forPlay: true,
-              params: page.params,
+              params: page.params.map((final e) {
+                if (paramsToSend?[e.id] != null) {
+                  if (paramsToSend?[e.id]['dataset'] == 'Text') {
+                    e.value = paramsToSend?[e.id]['label'];
+                  } else {
+                    final list = <DatasetObject>[
+                      DatasetObject(
+                        name: 'Parameters',
+                        map: currentPage.params
+                            .map((final e) => <String, dynamic>{e.name: e.get})
+                            .toList(),
+                      ),
+                      DatasetObject(
+                        name: 'States',
+                        map: currentPage.states
+                            .map((final e) => <String, dynamic>{e.name: e.get})
+                            .toList(),
+                      ),
+                      ...dataset,
+                    ];
+                    Map<String, dynamic>? selectedDataset;
+                    for (final element in list) {
+                      if (element.getName == paramsToSend?[e.id]['dataset']) {
+                        selectedDataset = <String, dynamic>{
+                          'name': element.getName,
+                          'map': element.getMap
+                        };
+                      }
+                    }
+                    VariableObject? variable;
+                    if (selectedDataset?['name'] == 'Parameters' ||
+                        selectedDataset?['name'] == 'States') {
+                      final map =
+                          selectedDataset!['map'] as List<Map<String, dynamic>>;
+                      //final element = map[loop ?? 0];
+                      for (final element in map) {
+                        if (element.keys.toList()[loop ?? 0] ==
+                            paramsToSend?[e.id]?['label']) {
+                          if (element[element.keys.toList()[loop ?? 0]]
+                              is String) {
+                            variable = VariableObject(
+                              name: element.keys.toList()[loop ?? 0],
+                              value: element[element.keys.toList()[loop ?? 0]],
+                            );
+                          }
+                          if (element[element.keys.toList()[loop ?? 0]]
+                              is CameraController) {
+                            variable = VariableObject(
+                              name: element.keys.toList()[loop ?? 0],
+                              type: VariableType.cameraController,
+                              controller:
+                                  element[element.keys.toList()[loop ?? 0]]
+                                      as CameraController,
+                            );
+                          }
+                          if (element[element.keys.toList()[loop ?? 0]]
+                              is XFile) {
+                            debugPrint('is XFile');
+                            variable = VariableObject(
+                              name: element.keys.toList()[loop ?? 0],
+                              type: VariableType.file,
+                              file: element[element.keys.toList()[loop ?? 0]]
+                                  as XFile,
+                            );
+                          }
+                        }
+                      }
+                    } else {
+                      final el =
+                          selectedDataset!['map'] as List<Map<String, dynamic>>;
+                      final map = el[loop ?? 0];
+                      for (final key in map.keys) {
+                        if (key == paramsToSend?[e.id]?['label']) {
+                          variable = VariableObject(name: key, value: map[key]);
+                        }
+                      }
+                    }
+                    if (variable != null) {
+                      e
+                        ..value = variable.get
+                        ..file = variable.file
+                        ..controller = variable.controller;
+                    }
+                  }
+                  return e;
+                }
+                return e;
+              }).toList(),
               states: page.states,
               dataset: page.datasets,
             ),
--- teta_widgets/lib/src/elements/actions/navigation/open_snack.dart
+++ teta_widgets/lib/src/elements/actions/navigation/open_snack.dart
@@ -2,6 +2,8 @@
 // ignore_for_file: public_member_api_docs, avoid_dynamic_calls
 
 // Flutter imports:
+import 'package:camera/camera.dart';
+import 'package:collection/collection.dart';
 import 'package:diacritic/diacritic.dart';
 import 'package:flutter/material.dart';
 // Package imports:
@@ -12,7 +14,11 @@ import 'package:teta_core/src/blocs/focus_project/index.dart';
 import 'package:teta_core/src/models/dataset.dart';
 import 'package:teta_core/src/models/page.dart';
 import 'package:teta_core/src/models/variable.dart';
+import 'package:teta_core/src/rendering/nodes_original.dart';
+import 'package:teta_core/src/repositories/queries/node.dart';
+import 'package:teta_core/teta_core.dart';
 import 'package:teta_widgets/src/elements/code/formatter_test.dart';
+import 'package:teta_widgets/src/elements/index.dart';
 
 class FActionNavigationOpenSnackBar {
   static Future action(
@@ -28,17 +34,120 @@ class FActionNavigationOpenSnackBar {
       if (nameOfPage != null) {
         final prj =
             BlocProvider.of<FocusProjectBloc>(context).state as ProjectLoaded;
-        final page = prj.prj.pages!
-            .firstWhere((final element) => element.name == nameOfPage);
-        final snackBar = SnackBar(
-          content: page.scaffold!.toWidget(
-            forPlay: true,
-            params: page.params,
-            states: page.states,
-            dataset: page.datasets,
-          ),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        final currentPage = BlocProvider.of<PageCubit>(context).state;
+        PageObject? page;
+        page = prj.prj.pages!
+            .firstWhereOrNull((final element) => element.name == nameOfPage);
+        if (page != null) {
+          final list = await NodeQueries.fetchNodesByPage(page.id);
+          final nodes = <CNode>[];
+          for (final e in list) {
+            nodes.add(
+              CNode.fromJson(
+                e as Map<String, dynamic>,
+                page.id,
+              ),
+            );
+          }
+          final scaffold = NodeRendering.renderTree(nodes);
+          page = page.copyWith(flatList: nodes, scaffold: scaffold);
+          final snackBar = SnackBar(
+            content: page.scaffold!.toWidget(
+              forPlay: true,
+              params: page.params.map((final e) {
+                if (paramsToSend?[e.id] != null) {
+                  if (paramsToSend?[e.id]['dataset'] == 'Text') {
+                    e.value = paramsToSend?[e.id]['label'];
+                  } else {
+                    final list = <DatasetObject>[
+                      DatasetObject(
+                        name: 'Parameters',
+                        map: currentPage.params
+                            .map((final e) => <String, dynamic>{e.name: e.get})
+                            .toList(),
+                      ),
+                      DatasetObject(
+                        name: 'States',
+                        map: currentPage.states
+                            .map((final e) => <String, dynamic>{e.name: e.get})
+                            .toList(),
+                      ),
+                      ...dataset,
+                    ];
+                    Map<String, dynamic>? selectedDataset;
+                    for (final element in list) {
+                      if (element.getName == paramsToSend?[e.id]['dataset']) {
+                        selectedDataset = <String, dynamic>{
+                          'name': element.getName,
+                          'map': element.getMap
+                        };
+                      }
+                    }
+                    VariableObject? variable;
+                    if (selectedDataset?['name'] == 'Parameters' ||
+                        selectedDataset?['name'] == 'States') {
+                      final map =
+                          selectedDataset!['map'] as List<Map<String, dynamic>>;
+                      //final element = map[loop ?? 0];
+                      for (final element in map) {
+                        if (element.keys.toList()[loop ?? 0] ==
+                            paramsToSend?[e.id]?['label']) {
+                          if (element[element.keys.toList()[loop ?? 0]]
+                              is String) {
+                            variable = VariableObject(
+                              name: element.keys.toList()[loop ?? 0],
+                              value: element[element.keys.toList()[loop ?? 0]],
+                            );
+                          }
+                          if (element[element.keys.toList()[loop ?? 0]]
+                              is CameraController) {
+                            variable = VariableObject(
+                              name: element.keys.toList()[loop ?? 0],
+                              type: VariableType.cameraController,
+                              controller:
+                                  element[element.keys.toList()[loop ?? 0]]
+                                      as CameraController,
+                            );
+                          }
+                          if (element[element.keys.toList()[loop ?? 0]]
+                              is XFile) {
+                            debugPrint('is XFile');
+                            variable = VariableObject(
+                              name: element.keys.toList()[loop ?? 0],
+                              type: VariableType.file,
+                              file: element[element.keys.toList()[loop ?? 0]]
+                                  as XFile,
+                            );
+                          }
+                        }
+                      }
+                    } else {
+                      final el =
+                          selectedDataset!['map'] as List<Map<String, dynamic>>;
+                      final map = el[loop ?? 0];
+                      for (final key in map.keys) {
+                        if (key == paramsToSend?[e.id]?['label']) {
+                          variable = VariableObject(name: key, value: map[key]);
+                        }
+                      }
+                    }
+                    if (variable != null) {
+                      e
+                        ..value = variable.get
+                        ..file = variable.file
+                        ..controller = variable.controller;
+                    }
+                  }
+                  return e;
+                }
+                return e;
+              }).toList(),
+              states: page.states,
+              dataset: page.datasets,
+            ),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        }
       }
     } catch (e) {
       debugPrint('$e');
