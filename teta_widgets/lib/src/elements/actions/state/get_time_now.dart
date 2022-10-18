971676b3a590470f6a2452d387274f3a3e8f8ccc
andreabuttarelli <andreabuttarelli.design@gmail.com>
2022-10-17 22:46:05 -0400

    feat(camera v)

---
+++ teta_widgets/lib/src/elements/actions/state/get_time_now.dart
@@ -0,0 +1,100 @@
+// Flutter imports:
+// ignore_for_file: public_member_api_docs, lines_longer_than_80_chars
+
+// Flutter imports:
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+// Package imports:
+import 'package:recase/recase.dart';
+import 'package:teta_core/teta_core.dart';
+import 'package:teta_widgets/src/elements/actions/snippets/get_page_on_code.dart';
+// Project imports:
+import 'package:teta_widgets/src/elements/actions/snippets/take_state_from.dart';
+import 'package:teta_widgets/src/elements/actions/snippets/update.dart';
+import 'package:teta_widgets/src/elements/index.dart';
+
+class FActionStateGetTimeHour {
+  static Future action({
+    required final BuildContext context,
+    required final List<VariableObject> params,
+    required final List<VariableObject> states,
+    required final List<DatasetObject> datasets,
+    required final String? stateName,
+    final int loop = 0,
+  }) async {
+    try {
+      final index =
+          states.indexWhere((final element) => element.name == stateName);
+      if (index >= 0) {
+        states[index].value = DateTime.now().hour.toString();
+        update(context);
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        // ignore: avoid_print
+        print(e);
+      }
+    }
+  }
+
+  static String toCode({
+    required final int pageId,
+    required final BuildContext context,
+    required final String? stateName,
+    required final FTextTypeInput? valueToChangeWith,
+    final bool isValueDefault = false,
+    final int loop = 0,
+  }) {
+    if (valueToChangeWith != null && stateName != null) {
+      final page = getPageOnToCode(pageId, context);
+      if (page == null) return '';
+      final variable = takeStateFrom(page, stateName);
+      if (variable == null) return '';
+
+      final varName = ReCase(stateName).camelCase;
+
+      final value = valueToChangeWith.toCode(
+        loop,
+        resultType: ResultTypeEnum.string,
+      );
+
+      if (isValueDefault) {
+        return '''
+          setState(() {
+            $varName = value;
+          });
+        ''';
+      }
+
+      final buffer = StringBuffer()..write('');
+      //this will be inserted in a onChange (string value) method so this should work like this
+      if (variable.type == VariableType.string) {
+        buffer.write(
+          '$varName = DateTime.now().hour.toString();',
+        );
+      } else {
+        if (variable.type == VariableType.int) {
+          final v = value.replaceAll("'", '');
+          Logger.printWarning(v);
+          buffer.write(
+            '$varName = ${int.tryParse(v) ?? v};',
+          );
+        } else if (variable.type == VariableType.double) {
+          final v = value.replaceAll("'", '');
+          buffer.write(
+            '$varName = ${double.tryParse(v) ?? v};',
+          );
+        } else {
+          return '';
+        }
+      }
+
+      return '''
+    setState(() {
+      ${buffer.toString()}
+    });
+    ''';
+    }
+    return '';
+  }
+}
---
+++ teta_widgets/lib/src/elements/widgets/components/login_section.dart
@@ -0,0 +1,105 @@
+// Flutter imports:
+import 'package:auth_buttons/auth_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+// Package imports:
+import 'package:teta_core/teta_core.dart';
+// Project imports:
+import 'package:teta_widgets/src/elements/index.dart';
+
+// Project imports:
+
+// ignore_for_file: public_member_api_docs
+
+class WLoginSection extends StatelessWidget {
+  /// Returns a [Align] widget in Teta
+  const WLoginSection(
+    final Key? key, {
+    required this.node,
+    required this.title,
+    required this.description,
+    required this.isAppleEnabled,
+    required this.isGitHubEnabled,
+    required this.isGoogleEnabled,
+    required this.textStyle,
+    required this.width,
+    required this.height,
+    required this.forPlay,
+    required this.params,
+    required this.states,
+    required this.dataset,
+    this.child,
+    this.loop,
+  }) : super(key: key);
+
+  final CNode node;
+  final CNode? child;
+  final FTextTypeInput title;
+  final FTextTypeInput description;
+  final bool isAppleEnabled;
+  final bool isGoogleEnabled;
+  final bool isGitHubEnabled;
+  final FSize width;
+  final FSize height;
+  final FTextStyle textStyle;
+  final bool forPlay;
+  final int? loop;
+
+  final List<VariableObject> params;
+  final List<VariableObject> states;
+  final List<DatasetObject> dataset;
+
+  @override
+  Widget build(final BuildContext context) {
+    final w = width.get(context: context, isWidth: true);
+    final h = height.get(context: context, isWidth: true);
+    return NodeSelectionBuilder(
+      node: node,
+      forPlay: forPlay,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextBuilder(
+            textStyle: textStyle,
+            value: title,
+            params: params,
+            states: states,
+            dataset: dataset,
+            forPlay: forPlay,
+            loop: loop,
+          ),
+          if (description
+              .get(params, states, dataset, forPlay, loop, context)
+              .isNotEmpty)
+            const Gap(4),
+          if (description
+              .get(params, states, dataset, forPlay, loop, context)
+              .isNotEmpty)
+            TextBuilder(
+              textStyle: textStyle,
+              value: description,
+              params: params,
+              states: states,
+              dataset: dataset,
+              forPlay: forPlay,
+              loop: loop,
+            ),
+          const Gap(Grid.medium),
+          if (isAppleEnabled)
+            AppleAuthButton(
+              style: AuthButtonStyle(width: w, height: h),
+            ),
+          if (isGoogleEnabled)
+            AppleAuthButton(
+              style: AuthButtonStyle(width: w, height: h),
+            ),
+          if (isGitHubEnabled)
+            AppleAuthButton(
+              style: AuthButtonStyle(width: w, height: h),
+            ),
+        ],
+      ),
+    );
+  }
+}
