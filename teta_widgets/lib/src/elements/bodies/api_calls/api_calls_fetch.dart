0416b21f76d83dacbe53e0b0162162d420bbdd03
andreabuttarelli <andreabuttarelli.design@gmail.com>
2022-12-09 17:08:18 +0100

    Update api_calls_fetch.dart

--- teta_widgets/lib/src/elements/bodies/api_calls/api_calls_fetch.dart
+++ teta_widgets/lib/src/elements/bodies/api_calls/api_calls_fetch.dart
@@ -47,7 +47,7 @@ final apiCallsFetchIntrinsicStates = IntrinsicStates(
   advicedChildrenCanHaveAtLeastAChild: [],
   displayName: NodeType.name(_globalType),
   type: _globalType,
-  category: NodeCategories.basic,
+  category: NodeCategories.experimental,
   maxChildren: 2,
   canHave: ChildrenEnum.children,
   addChildLabels: [
