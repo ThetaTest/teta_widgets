4e6c5eda3a7ec330a4f0ae0b4f1f74e7b366afd4
Andrea Buttarelli <49411143+andreabuttarelli@users.noreply.github.com>
2023-03-28 11:38:52 +0200

    removing models

--- theta_widgets/lib/src/elements/features/actions/enums/airtable.dart
+++
@@ -1,5 +0,0 @@
-enum ActionAirtableDB {
-  insert,
-  delete,
-  update
-}
--- theta_widgets/lib/src/elements/features/actions/enums/index.dart
+++
@@ -1,5 +0,0 @@
-export 'gestures.dart';
-export 'navigation.dart';
-export 'state.dart';
-export 'supabase.dart';
-export 'type.dart';
